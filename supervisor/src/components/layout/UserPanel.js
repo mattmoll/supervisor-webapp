f37414b85debb0b344d48ec68f74f7163e7a83b9
Matt <matias_moll@yahoo.com.ar>
2020-07-12 20:21:09 -0300

    Added some placeholder ideas for the user settings components.

--- supervisor/src/components/layout/UserPanel.js
+++ supervisor/src/components/layout/UserPanel.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 
 export default class UserPanel extends Component {
+  state = {
+    username: "Matias",
+  };
   render() {
     return (
       <div id="user-panel">
-        <h4>User settings</h4>
+        <h4>Settings</h4>
+        <hr />
+        <div>
+          <div>Username: {this.state.username}</div>
+          <div>Theme: Light/Dark</div>
+          <div>LogOut</div>
+        </div>
       </div>
     );
   }
