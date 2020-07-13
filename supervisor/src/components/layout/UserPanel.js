3735a5bef40c82c9db6e426a649329054df5dc20
Matt <matias_moll@yahoo.com.ar>
2020-07-13 15:37:14 -0300

    Added styles and components to User Settings widget.

--- supervisor/src/components/layout/UserPanel.js
+++ supervisor/src/components/layout/UserPanel.js
@@ -4,15 +4,38 @@ export default class UserPanel extends Component {
   state = {
     username: "Matias",
   };
+
+  toggleTheme = (e) => {
+    console.log("TODO: Toggled Theme");
+  };
+
   render() {
     return (
       <div id="user-panel">
-        <h4>Settings</h4>
+        <h4 className="mt-2 ml-3 mb-0">Settings</h4>
         <hr />
         <div>
-          <div>Username: {this.state.username}</div>
-          <div>Theme: Light/Dark</div>
-          <div>LogOut</div>
+          <div className="settings-item">
+            <i className="fas fa-user mr-2"></i> User:
+            <div className="setting-value ml-2">{this.state.username}</div>
+          </div>
+          <div className="settings-item">
+            <i className="fas fa-adjust mr-2"></i> Theme:
+            <div className="setting-value ml-2" onClick={this.toggleTheme}>
+              <label className="switch">
+                <input type="checkbox" />
+                <span className="slider round"></span>
+              </label>
+            </div>
+          </div>
+          <div className="settings-item">
+            <i className="fas fa-sign-out-alt mr-2"></i> LogOut:
+            <div className="setting-value ml-2">
+              <button type="button" className="btn btn-outline-dark btn-sm">
+                Exit
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     );
