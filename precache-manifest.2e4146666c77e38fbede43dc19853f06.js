b67963fafceb54e73b6f3b5b69e28d1e163bfd46
Matt <codejourney.contact@gmail.com>
2020-12-10 00:47:16 -0300

    Updates

---
+++ precache-manifest.2e4146666c77e38fbede43dc19853f06.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7299f082d94eae4741e9588133c84a6",
+    "url": "/supervisor-webapp/index.html"
+  },
+  {
+    "revision": "e35686e06ac9c6b49ae0",
+    "url": "/supervisor-webapp/static/css/main.1a9eb83a.chunk.css"
+  },
+  {
+    "revision": "d305e00c94bcd5aac53d",
+    "url": "/supervisor-webapp/static/js/3.89c24575.chunk.js"
+  },
+  {
+    "revision": "52fe152fa8b4fffdb1e1",
+    "url": "/supervisor-webapp/static/js/4.484bec2c.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/supervisor-webapp/static/js/4.484bec2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41d452f672e86698b740",
+    "url": "/supervisor-webapp/static/js/5.a770b57d.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/supervisor-webapp/static/js/5.a770b57d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8db5133d4ebd93eaca52",
+    "url": "/supervisor-webapp/static/js/6.624cd206.chunk.js"
+  },
+  {
+    "revision": "e3b93feea552d5fd932a1758b1da5f1a",
+    "url": "/supervisor-webapp/static/js/6.624cd206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e35686e06ac9c6b49ae0",
+    "url": "/supervisor-webapp/static/js/main.3b19a8c9.chunk.js"
+  },
+  {
+    "revision": "c9488589cf8e1588b987",
+    "url": "/supervisor-webapp/static/js/runtime-main.0f80f6da.js"
+  },
+  {
+    "revision": "67b5ca27b02d5a78a3e9",
+    "url": "/supervisor-webapp/static/js/xlsx.0200220d.chunk.js"
+  },
+  {
+    "revision": "d1c5834fab907980bf6cc8c497d1541b",
+    "url": "/supervisor-webapp/static/media/background3.d1c5834f.jpg"
+  },
+  {
+    "revision": "d4d5345841cf7a7a2e71e4a61a8d18c9",
+    "url": "/supervisor-webapp/static/media/icon.d4d53458.png"
+  }
+]);
---
+++ static/js/xlsx.0200220d.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpsupervisor=this.webpackJsonpsupervisor||[]).push([[2],{282:function(s,n){},371:function(s,n){},372:function(s,n){}}]);
+//# sourceMappingURL=xlsx.0200220d.chunk.js.map
